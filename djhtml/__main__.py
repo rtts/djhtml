a51ac0bbb69aabdaee269597bb38045292089e47
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2021-05-11 20:00:29 +0200

    Extract indenting logic to separate function.
    Closes #1.

--- djhtml/__main__.py
+++ djhtml/__main__.py
@@ -1,6 +1,6 @@
 import sys
 
-from .modes import HTML
+from .djhtml import djhtml_indent
 
 
 def main():
@@ -10,18 +10,12 @@ def main():
         print("Usage: djhtml <file-to-indent>")
         sys.exit(1)
 
-    current_mode = HTML
-    current_level = 0
-
     with open(filename, "r") as f:
         lines = f.readlines()
 
     with open(filename, "w") as f:
-        for line in lines:
-            mode = current_mode(line.rstrip())
-            f.write(mode.get_line(current_level) + "\n")
-            current_level += mode.nextlevel
-            current_mode = mode.nextmode
+        for line in djhtml_indent(lines):
+            f.write(line)
 
 
 if __name__ == "__main__":
