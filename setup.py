afca3d79c0bba3dd47edd1174a439f6a7b753ed3
Jaap Joris Vens <jj@rtts.eu>
2021-05-13 00:58:46 +0200

    Python packaging done right™

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
 #!/usr/bin/env python3
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="djhtml",
-    version="0.0.3",
-    author="Jaap Joris Vens",
-    author_email="jj@rtts.eu",
-    description="Django template indenter",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rtts/djhtml",
-    packages=["djhtml"],
-    entry_points={
-        "console_scripts": ["djhtml=djhtml.__main__:main"],
-    },
-    include_package_data=True,
-    license="GPLv3+",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",
-)
+if __name__ == "__main__":
+    setuptools.setup()
