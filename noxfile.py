34f162699c94009e9a104d829f202c0f57007d10
Adam Johnson <me@adamj.eu>
2021-11-08 11:59:02 +0000

    Support Python 3.10

--- noxfile.py
+++ noxfile.py
@@ -19,6 +19,6 @@ def lint_flake8(session):
     session.run("python", "-m", "flake8", ".")
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
 def tests(session):
     session.run("python", "-m", "unittest", "discover", "-v")
