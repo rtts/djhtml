2443375bed10f837643bd9574cee77594a6a2f2e
Jaap Joris Vens <jj@rtts.eu>
2023-02-19 22:36:22 +0100

    Skip empty lines when guessing tabwidth

--- djhtml/__main__.py
+++ djhtml/__main__.py
@@ -70,7 +70,7 @@ def main():
                         count += 4
                     else:
                         break
-                if 0 < abs(count - diff) < 8:
+                if count and 0 < abs(count - diff) < 8:
                     probabilities[abs(count - diff)] += 1
                 diff = count
             guess = probabilities.index(max(probabilities))
