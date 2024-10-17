d50d3d9908e6903b4b0eb806f36d5ad1200f5cbe
Jaap Joris Vens <jj@rtts.eu>
2024-10-17 20:35:42 +0200

    Drop support for Python 3.8, add support for Python 3.13

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def tests(session):
     session.run("python", "-m", "unittest")
