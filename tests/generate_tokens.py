af8fb3d7b0004a29cc92c7234267590c77103920
Jaap Joris Vens <jj@rtts.eu>
2025-05-22 09:28:16 +0200

    Fix `generate_tokens.py`

--- tests/generate_tokens.py
+++ tests/generate_tokens.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Generate *.tokens files from *.html files in the suite directory.
-
 """
 
 from pathlib import Path
@@ -14,4 +13,4 @@ for filename in DIR.iterdir():
     if filename.suffix == ".html":
         with open(DIR / filename) as html:
             with open(DIR / (filename.stem + ".tokens"), "w") as f:
-                f.write(DjHTML(html.read()).debug())
+                f.write(DjHTML(html.read(), extra_blocks=[("weird_tag", "endweird")]).debug())
