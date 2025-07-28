b813040b7958e8262774d0c370b8b172b8c13635
Jaap Joris Vens <jj@rtts.eu>
2025-07-28 16:56:01 +0200

    Get rid of redundant `options.pyi`

--- djhtml/options.py
+++ djhtml/options.py
@@ -10,6 +10,16 @@ import argparse
 import sys
 from importlib.metadata import version
 
+tabwidth: int
+input_filenames: list[str]
+check: bool
+show_help: bool
+show_version: bool
+debug: bool
+in_place: bool
+extra_block: list[tuple[str, str]] | None
+
+
 parser = argparse.ArgumentParser(
     description=(
         """
--- djhtml/options.pyi
+++
@@ -1,8 +0,0 @@
-tabwidth: int
-input_filenames: list[str]
-check: bool
-show_help: bool
-show_version: bool
-debug: bool
-in_place: bool
-extra_block: list[tuple[str, str]] | None
